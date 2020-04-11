3a83358f0a144158922ea42344cc13e1c9926ccf
Safi <safiya.valieva@gmail.com>
2020-04-11 19:48:24 +0300

    сат

---
+++ Scene/Assets/Scripts/Characters Controllers/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(MoveController))]
+public class PlayerController : MonoBehaviour
+{
+    MoveController moveController;
+    //Transform characterTransform;
+    void Start()
+    {
+        moveController = GetComponent<MoveController>();
+        //characterTransform = this.transform;
+    }
+
+    private void FixedUpdate()
+    {
+        moveController.CharacterMove();
+        moveController.CharacterRot();
+        moveController.Jump();
+        moveController.Fly();
+    }
+}
