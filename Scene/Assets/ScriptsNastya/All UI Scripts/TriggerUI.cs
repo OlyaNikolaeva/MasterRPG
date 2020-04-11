f9c42f16557dc128919488fbe932b73bd93782a3
Galichanina_Anastasiya_11-708 <asyagalich29@gmail.com>
2020-04-11 19:49:41 +0300

    Перенесла

---
+++ Scene/Assets/ScriptsNastya/All UI Scripts/TriggerUI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerUI : MonoBehaviour
+{
+    public GameObject QuestUI;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player" & Input.GetKey(KeyCode.Space))
+            QuestUI.SetActive(true);
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+            QuestUI.SetActive(false);
+
+
+    }
+}
