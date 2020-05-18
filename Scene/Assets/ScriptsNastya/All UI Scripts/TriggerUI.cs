6af62b2d41653430aaacbeab9ac02c40eea3b80a
Master_RPG <https://github.com/Karabaeva/Master_RPG>
2020-05-18 20:30:00 +0300

    Update

--- Scene/Assets/ScriptsNastya/All UI Scripts/TriggerUI.cs
+++ Scene/Assets/ScriptsNastya/All UI Scripts/TriggerUI.cs
@@ -4,19 +4,28 @@ using UnityEngine;
 
 public class TriggerUI : MonoBehaviour
 {
+    public GameObject TalktUI;
     public GameObject QuestUI;
 
     private void OnTriggerStay(Collider other)
     {
-        if (other.tag == "Player" & Input.GetKey(KeyCode.Space))
-            QuestUI.SetActive(true);
+        if (other.tag == "Player")
+        { TalktUI.SetActive(true);
+            if (other.tag == "Player" & Input.GetKeyDown("B"))
+                TalktUI.SetActive(false);
+                QuestUI.SetActive(true);
+        }
         
     }
 
     private void OnTriggerExit(Collider other)
     {
         if (other.tag == "Player")
+        {
+            TalktUI.SetActive(false);
             QuestUI.SetActive(false);
+        }
+            
 
 
     }
